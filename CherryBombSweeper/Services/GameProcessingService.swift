238bf8d5a591ab81b3a08e44db2a73adc7dd4841
Duy Nguyen <duy@weareenvoy.com>
2018-01-17 16:40:41 -0800

    Implemented pre-calculation of empty cell clusters to reduce runtime calculation during game play when a large empty cell is tapped.

--- CherryBombSweeper/Services/GameProcessingService.swift
+++ CherryBombSweeper/Services/GameProcessingService.swift
@@ -9,15 +9,15 @@
 import Foundation
 
 protocol GameStatusListener {
-    func onCellReveal(_ revealedCells: [Int]) -> Void
-    func onCellHighlight(_ highlightedCells: [Int]) -> Void
+    func onCellReveal(_ revealedCells: Set<Int>) -> Void
+    func onCellHighlight(_ highlightedCells: Set<Int>) -> Void
     func onCellFlagged(_ flaggedCell: Int) -> Void
     func onCellUnflagged(_ unflaggedCell: Int) -> Void
     func onCellExploded(_ explodedCell: Int) -> Void
     func onGameCompleted() -> Void
 }
 
-typealias CellRevealHandler = (_ revealedCells: [Int]) -> Void
+typealias CellRevealHandler = (_ revealedCells: Set<Int>) -> Void
 
 class GameProcessingService: NSObject {
     
@@ -39,36 +39,36 @@ class GameProcessingService: NSObject {
             if let cell = game.mineField.getCell(at: cellIndex) {
                 switch userAction {
                 case .tap:
-                    self.tapCell(at: cell)
+                    self.tap(at: cell)
                 case .flag:
-                    self.flagCell(at: cell)
+                    self.flag(at: cell)
                 }
             }
         }
     }
     
-    private func tapCell(at cell: Cell) {
+    private func tap(at cell: Cell) {
         if cell.state == .flagged {
-            self.flagCell(at: cell)
+            self.flag(at: cell)
         } else if cell.state == .revealed, !cell.isEmpty {
-            self.probeCell(at: cell)
+            self.probe(at: cell)
         } else if cell.hasBomb {
-            self.explodeCell(at: cell)
+            self.explode(at: cell)
         } else {
-            self.revealCell(at: cell.id) { (revealedCells) in
+            self.reveal(at: cell.index) { (revealedCells) in
                 self.gameListener?.onCellReveal(revealedCells)
             }
         }
     }
     
-    private func revealCell(at cellIndex: Int, revealCellHandler: @escaping CellRevealHandler) {
+    private func reveal(at cellIndex: Int, revealCellHandler: @escaping CellRevealHandler) {
         self.processingQueue.async {
             if let cell = self.currentGame?.mineField.getCell(at: cellIndex) {
                 
                 guard cell.state == .untouched else { return }
                 
                 if cell.hasBomb {
-                    self.explodeCell(at: cell)
+                    self.explode(at: cell)
                 } else {
                     cell.state = .revealed
                     
@@ -79,83 +79,91 @@ class GameProcessingService: NSObject {
                         self.gameListener?.onGameCompleted()
                     }
                     
-                    let revealedCell: [Int] = [cell.id]
                     
-                    if cell.isEmpty {
-                        
-                        revealCellHandler(revealedCell)
-                        
-                        self.revealEmptyAdjacentCells(to: cell, revealCellHandler: revealCellHandler)
+                    if cell.connectedEmptyCluster.isEmpty {
+                        revealCellHandler(Set([cell.index]))
                     } else {
-                        revealCellHandler(revealedCell)
+                        revealCellHandler(cell.connectedEmptyCluster)
                     }
+//
+//
+//                    let revealedCell: [Int] = [cell.index]
+//
+//                    if cell.isEmpty {
+//
+//                        revealCellHandler(revealedCell)
+//
+//                        self.revealEmptyAdjacentCells(to: cell, revealCellHandler: revealCellHandler)
+//                    } else {
+//                        revealCellHandler(revealedCell)
+//                    }
                 }
             }
         }
     }
     
-    private func revealEmptyAdjacentCells(to cell: Cell, revealCellHandler: @escaping CellRevealHandler) {
-        let emptyCellsId = cell.adjacentCellsCoordMap.map{ (cellId, _) in return cellId }.filter {
-            if let cell = self.currentGame?.mineField.getCell(at: $0), !cell.hasBomb, cell.state == .untouched {
-                return true
-            }
-            return false
-        }
-        
-        for cellId in emptyCellsId {
-            self.revealCell(at: cellId, revealCellHandler: revealCellHandler)
-        }
-    }
+//    private func revealEmptyAdjacentCells(to cell: Cell, revealCellHandler: @escaping CellRevealHandler) {
+//        let emptyCellIdices = cell.adjacentCellIndices.filter {
+//            if let cell = self.currentGame?.mineField.getCell(at: $0), !cell.hasBomb, cell.state == .untouched {
+//                return true
+//            }
+//            return false
+//        }
+//
+//        for cellIdex in emptyCellIdices {
+//            self.reveal(at: cellIdex, revealCellHandler: revealCellHandler)
+//        }
+//    }
     
-    private func explodeCell(at cell: Cell) {
+    private func explode(at cell: Cell) {
         guard cell.state == .untouched else { return }
         
         cell.state = .exploded
         
         self.currentGame?.mineField.updateCell(cell)
         
-        self.gameListener?.onCellExploded(cell.id)
+        self.gameListener?.onCellExploded(cell.index)
     }
     
-    private func probeCell(at cell: Cell) {
+    private func probe(at cell: Cell) {
         guard let mineField = self.currentGame?.mineField, cell.state == .revealed, !cell.isEmpty else { return }
         
-        let flaggedCellIDs = cell.adjacentCellsCoordMap.map{ (cellId, _) in return cellId }.filter {
+        let flaggedCellIDs = cell.adjacentCellIndices.filter {
             mineField.getCell(at: $0)?.state == .flagged
         }
         
-        let untouchedCellIDs = cell.adjacentCellsCoordMap.map{ (cellId, _) in return cellId }.filter {
+        let untouchedCellIDs = cell.adjacentCellIndices.filter {
             mineField.getCell(at: $0)?.state == .untouched
         }
         
         if flaggedCellIDs.count >= cell.adjacentBombs {
             for cellId in untouchedCellIDs {
-                self.revealCell(at: cellId) { (revealedCells) in
+                self.reveal(at: cellId) { (revealedCells) in
                     self.gameListener?.onCellReveal(revealedCells)
                 }
             }
         } else {
             // flash the cells
-            self.gameListener?.onCellHighlight(untouchedCellIDs)
+            self.gameListener?.onCellHighlight(Set(untouchedCellIDs))
         }
     }
     
-    private func flagCell(at cell: Cell) {
+    private func flag(at cell: Cell) {
         switch cell.state {
         case .revealed:
             if !cell.isEmpty {
-                self.probeCell(at: cell)
+                self.probe(at: cell)
             }
         case .untouched:
             cell.state = .flagged
             self.currentGame?.mineField.updateCell(cell)
             
-            self.gameListener?.onCellFlagged(cell.id)
+            self.gameListener?.onCellFlagged(cell.index)
         case .flagged:
             cell.state = .untouched
             self.currentGame?.mineField.updateCell(cell)
             
-            self.gameListener?.onCellUnflagged(cell.id)
+            self.gameListener?.onCellUnflagged(cell.index)
         default:
             break
         }
